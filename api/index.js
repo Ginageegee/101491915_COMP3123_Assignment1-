2f4bdb8075125b6933bb139bd444b6bc15efd9c9
Ginageegee <regina.slonimsky@hotmail.com>
2025-10-10 18:39:11 -0400

    final revision

--- api/index.js
+++ api/index.js
@@ -1,28 +1,21 @@
 const express = require('express');
+const serverless = require('serverless-http');
 require('dotenv').config();
-const connectDB = require('../config/db'); // Adjusted path for Vercel
+const connectDB = require('../config/db');
 
 const app = express();
 
-// Connect to MongoDB
 connectDB();
-
-// Middleware to parse JSON
 app.use(express.json());
 
-// Basic test route
 app.get('/', (req, res) => {
-    console.log('Root route hit');
     res.send('Your server is running on Vercel!');
 });
 
-// Mount employee routes
 const employeeRoutes = require('../routes/employeeRoutes');
 app.use('/api/v1/emp', employeeRoutes);
 
-// Mount user routes
 const userRoutes = require('../routes/userRoutes');
 app.use('/api/v1/user', userRoutes);
 
-// Export the app (no app.listen)
-module.exports = app;
+module.exports = serverless(app);
